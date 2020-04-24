7ef11c231d44ff999e714d571572eceee0d4f595
Pete Chappell <pchappell@intellum.com>
2020-04-24 15:43:02 +0100

    Initial commit (from create-react-app).

---
+++ src/components/App/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function App() {
+  return (
+    <main>
+      <header><h1>Find your learning style</h1></header>
+      <p>This questionnaire is designed to find out your preferred learning style(s).
+        Over the years you have probably developed learning &quot;habits&quot; that
+        help you benefit more from some experiences than from others. Since you are
+        probably unaware of this, this questionnaire will help you pinpoint your
+        learning preferences so that you are in a better position to select learning
+        experiences that suit your style and having a greater understanding of those
+        that suit the style of others.</p>
+      <p>This is an internationally proven tool designed by Peter Honey and Alan Mumford.</p>
+      <p>There is no time limit to this questionnaire. It will probably take you 10-15
+        minutes. The accuracy of the results depends on how honest you can be. There are
+        no right or wrong answers.</p>
+    </main>
+  );
+}
+
+export default App;
