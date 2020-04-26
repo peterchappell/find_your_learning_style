7da6715801b4ebaf5149b89043e160a536ef737d
Pete Chappell <pchappell@intellum.com>
2020-04-26 13:12:53 +0100

    Updates

---
+++ precache-manifest.229ce9c4670c3f8904f0e73f94042bfe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e314c366aaef08429b5425bda478dc1e",
+    "url": "/find_your_learning_style/index.html"
+  },
+  {
+    "revision": "458f849e9274c67ccdd3",
+    "url": "/find_your_learning_style/static/css/main.8a03ba0e.chunk.css"
+  },
+  {
+    "revision": "60c16a741f311b2784e3",
+    "url": "/find_your_learning_style/static/js/2.2257ecc2.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/find_your_learning_style/static/js/2.2257ecc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "458f849e9274c67ccdd3",
+    "url": "/find_your_learning_style/static/js/main.685baa14.chunk.js"
+  },
+  {
+    "revision": "1063e19128f6e0feaa07",
+    "url": "/find_your_learning_style/static/js/runtime-main.5f10fc22.js"
+  }
+]);
